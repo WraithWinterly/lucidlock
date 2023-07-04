11037988ce0433d4d8dd6682de3525654213e02e
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2023-07-04 04:22:28 -0400

    Fix header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   const { isSignedIn, isLoaded } = useAuth();
   return (
-    <div className="fixed left-0 right-0 top-0 bottom-0 flex justify-between p-2">
+    <div className="fixed left-0 right-0 top-0 h-12 flex justify-between p-2 bg-black/20 items-center">
       <Link href="/app">
         <span className="text-[hsl(280,100%,70%)] font-extrabold">
           Lucidlock
